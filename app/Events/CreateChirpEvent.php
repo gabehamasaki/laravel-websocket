19b142f01dee9f7bf74237c8c3c968d2d69a8375
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2024-03-10 22:45:00 -0300

    :tada: Init project

---
+++ app/Events/CreateChirpEvent.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Chirp;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class CreateChirpEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(protected Chirp $chirp)
+    {
+        //
+    }
+
+        /**
+         * Get the data to broadcast.
+         *
+         * @return array<string, mixed>
+         */
+        public function broadcastWith(): array
+        {
+            return ['chirp' => $this->chirp];
+        }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('chirps'),
+        ];
+    }
+}
---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\CreateChirpEvent;
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp = $request->user()->chirps()->create($validated);
+
+        CreateChirpEvent::dispatch($chirp);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
